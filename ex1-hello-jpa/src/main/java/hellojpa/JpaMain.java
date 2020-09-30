4bcf8bf98c385529a572b034f522a2450df2c749
eprj453 <eprj453@gmail.com>
2020-10-01 00:54:46 +0900

    201001_commit1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,44 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜잭션 얻어옴
+        EntityTransaction tx = em.getTransaction();
+
+        // 트랜잭션 시작
+        tx.begin();
+
+        try {
+            // Member 저장
+            Member member = em.find(Member.class, 1L);
+            List<Member> findMembers = em.createQuery("select m from Member m", Member.class).getResultList();
+            // commit
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback(); // 만약 commit전 문제가 생기면 rollback
+        } finally {
+            em.close();
+        }
+
+//        // Member 저장
+//        Member member = new Member();
+//        member.setId(2L);
+//        member.setName("hello-jpa");
+//        em.persist(member);
+//
+//        // 트랜잭션 commit
+//        tx.commit();
+//
+//        em.close();
+        emf.close();
     }
 }
